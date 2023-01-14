5ed6758c81b1c48fe75a1def1cb379e24801f97e
mateochumbita <mateo.chumbita@gmail.com>
2023-01-14 15:00:05 -0300

    Mejora Formularios

--- sistema_stock/sistema_stock/settings.py
+++ sistema_stock/sistema_stock/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'existencias',
+    
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
